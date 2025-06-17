b9b5dc904344f9892876fe6a66caa345497901c5
vk3005 <dvigneshvcp2@gmail.com>
2025-06-17 16:54:43 +0530

    Time: 1 ms (83.73%), Space: 41.6 MB (63.1%) - LeetHub

---
+++ 1960-check-if-the-sentence-is-pangram/1960-check-if-the-sentence-is-pangram.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean checkIfPangram(String sentence) {
+        boolean[] letters = new boolean[25];
+
+        for(char c = 'a';c<='z';c++){
+            if(!sentence.contains(String.valueOf(c)))
+                return false;
+        }
+            return true;
+        }
+    }
