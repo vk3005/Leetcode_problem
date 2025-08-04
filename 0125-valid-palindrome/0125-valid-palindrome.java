1a54c3fc562cecac78d3286ae3be474501c08a06
vk3005 <dvigneshvcp2@gmail.com>
2025-08-04 17:52:21 +0530

    Time: 13 ms (42.73%), Space: 45.1 MB (29.87%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -3,9 +3,8 @@ class Solution {
         s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
         int l = 0,r = s.length()-1;
         while(l<r){
-            if(s.charAt(l) != s.charAt(r)){
-                return false;
-            }
+            if(s.charAt(l) != s.charAt(r)) return false;
+            
             l++;
             r--;
         }
